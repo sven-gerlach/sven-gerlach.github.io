bd182c9e1cfbec50303148ce78a03effc49bac62
sven-gerlach <svengerlach@me.com>
2021-04-04 15:19:42 -0400

    Add simple navbar inside header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react'
+import Header from './components/Navbar/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+
+      </main>
+    </Fragment>
   );
 }
 
